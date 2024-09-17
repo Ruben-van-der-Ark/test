5da7a806532e89c33a2c247a1c503cb17f5f9180
Ruben-van-der-Ark <s1210076@student.windesheim.nl>
2024-09-17 10:49:15 +0200

    Three javascript related tests

---
+++ testjs.js
@@ -0,0 +1,11 @@
+function testInnerHTML(){
+    document.getElementById("test").innerHTML = "It worked!";
+}
+
+function testAlert() {
+    alert("Test successful!")
+}
+
+function testConsoleLog() {
+    console.log("Test successful!")
+}
