3b6ffed75a7f2b41b429167d3eadd5f2498ff07c
Ruben-van-der-Ark <s1210076@student.windesheim.nl>
2024-09-17 11:30:28 +0200

    Rickroll test

--- testjs.js
+++ testjs.js
@@ -9,3 +9,7 @@ function testAlert() {
 function testConsoleLog() {
     console.log("Test successful!")
 }
+
+function testRickRoll() {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
