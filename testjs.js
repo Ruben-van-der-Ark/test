d2fc6fe06a598af2d700da5c9f0aaca1682961cd
Ruben-van-der-Ark <s1210076@student.windesheim.nl>
2024-09-17 11:57:08 +0200

    For loop tests

--- testjs.js
+++ testjs.js
@@ -1,15 +1,28 @@
+// via w3schools en zelf expirimenteren
 function testInnerHTML(){
     document.getElementById("test").innerHTML = "Test successful!";
 }
 
 function testAlert() {
-    alert("Test successful!")
+    alert("Test successful!");
 }
 
 function testConsoleLog() {
-    console.log("Test successful!")
+    console.log("Test successful!");
 }
 
 function testRickRoll() {
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+
+function testForLoop() {
+    for (let i=1; i<=5; i++) {
+        console.log('*'.repeat(i));
+    }
+
+    for (let i=1; i<=5; i++) {
+        for (let j=1; j<=5; j++) {
+            console.log(i, j)
+        }
+    }
 }
