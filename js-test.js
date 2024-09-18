536cdca07d856d65a7b7387b879d4d75e564d1fe
Ruben-van-der-Ark <s1210076@student.windesheim.nl>
2024-09-18 12:17:48 +0200

    More tests regarding input and formatting

--- js-test.js
+++ js-test.js
@@ -36,26 +36,41 @@ function testForLoop() {
 }
 
 function testDraggable() {
-    let dragdiv = document.getElementById('test-draggable-div')
+    let dragdiv = document.getElementById('test-draggable-div');
     dragdiv.draggable = !dragdiv.draggable;
-    let dragbool = dragdiv.draggable
-    document.getElementById('test-draggable-bool').innerHTML =
+    let dragbool = dragdiv.draggable;
+    document.getElementById('out-draggable').innerHTML =
         dragbool ? "Draggable" : "Not draggable";
 }
 
-function testParams(color) {
+function testEqualInputs() {
+    let input1 = document.getElementById("test-input1");
+    let input2 = document.getElementById("test-input2");
+    document.getElementById('out-equal-inputs').innerHTML =
+        input1.value === input2.value ? "Equal" : "Not equal";
+}
+
+function testInputLength() {
+    let input = document.getElementById("test-input-length");
+    document.getElementById("out-input-length").innerHTML =
+        input.value.length;
+}
+
+function testColor(color) {
     let colordiv = document.getElementById('test-color')
-    switch (color) {
-        case 'red':
-            colordiv.style.backgroundColor = "#FF0000";
-            break;
-        case 'orange':
-            colordiv.style.backgroundColor = "#FFAA00";
-            break;
-        case 'green':
-            colordiv.style.backgroundColor = "#00FF00";
-            break;
-        default:
-            break;
-    }
+    // switch (color) {
+    //     case 'red':
+    //         colordiv.style.backgroundColor = "#FF0000";
+    //         break;
+    //     case 'orange':
+    //         colordiv.style.backgroundColor = "#FFAA00";
+    //         break;
+    //     case 'green':
+    //         colordiv.style.backgroundColor = "#00FF00";
+    //         break;
+    //     default:
+    //         break;
+    // }
+    let colors = {'red': "#FF0000", 'orange': "#FFAA00", 'green': "#00FF00"};
+    colordiv.style.backgroundColor = colors[color];
 }
